9fce201343cebfd19e55e0eef998991b6e61cd69
Cathbhadh <36742410+Cathbhadh@users.noreply.github.com>
2024-02-27 21:05:56 +0200

    Update likes.py

--- likes.py
+++ likes.py
@@ -48,12 +48,13 @@ if access_token:
     if st.button("Load Data"):
         load_data()
 
-        # Convert dictionaries to pandas dataframes
-        df_likes = pd.DataFrame(list(user_likes.items()), columns=['User', 'Likes'])
-        df_comments = pd.DataFrame(list(user_comments.items()), columns=['User', 'Comments'])
+        df_likes = pd.DataFrame(list(user_likes.items()), columns=["User", "Likes"])
+        df_comments = pd.DataFrame(
+            list(user_comments.items()), columns=["User", "Comments"]
+        )
 
-        total_likes = df_likes['Likes'].sum()
-        total_comments = df_comments['Comments'].sum()
+        total_likes = df_likes["Likes"].sum()
+        total_comments = df_comments["Comments"].sum()
 
         st.subheader("Total Likes and Comments")
         st.write(f"Total Likes: {total_likes}")
@@ -63,28 +64,33 @@ if access_token:
 
         with col1:
             st.subheader("Likes by user:")
-            st.dataframe(df_likes.sort_values(by='Likes', ascending=False))
+            st.dataframe(df_likes.sort_values(by="Likes", ascending=False))
 
         with col2:
             st.subheader("Comments by user:")
-            st.dataframe(df_comments.sort_values(by='Comments', ascending=False))
-
+            st.dataframe(df_comments.sort_values(by="Comments", ascending=False))
 
         average_likes_per_user = total_likes / len(df_likes)
         st.subheader("Average Likes per User")
         st.write(f"Average Likes per User: {average_likes_per_user:.2f}")
 
-        st.subheader("User Percentile Analysis")
-
+        st.subheader("Percentile:")
         percentiles = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
         percentiles_values_likes = np.percentile(df_likes['Likes'], percentiles)
         percentiles_values_comments = np.percentile(df_comments['Comments'], percentiles)
 
-        for percentile, value in zip(percentiles, percentiles_values_likes):
-            st.write(f"{percentile}th percentile Likes: {value}")
+        col1, col2 = st.columns(2)
 
-        for percentile, value in zip(percentiles, percentiles_values_comments):
-            st.write(f"{percentile}th percentile Comments: {value}")
+        with col1:
+            st.subheader("Likes Percentiles")
+            for percentile, value in zip(percentiles, percentiles_values_likes):
+                st.write(f"{percentile}th percentile: {value}")
+
+        with col2:
+            st.subheader("Comments Percentiles")
+            for percentile, value in zip(percentiles, percentiles_values_comments):
+                st.write(f"{percentile}th percentile: {value}")
+
 
 else:
     st.warning("Please enter your access token")
