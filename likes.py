6d1118014f9cbcc14d13994c49cd0e966fd424df
Cathbhadh <36742410+Cathbhadh@users.noreply.github.com>
2024-03-19 16:33:57 +0200

    Update likes.py

--- likes.py
+++ likes.py
@@ -78,20 +78,17 @@ def analyze_likes(user_likes, followers, follower_like_counts):
     total_followers = len(follower_names)
     st.write(f"{users_with_no_likes_count} ({users_with_no_likes_count/total_followers*100:.2f}%) out of {total_followers} followers didn't leave any likes")
 
-    if follower_like_counts.dtypes != 'int64':
-        st.warning("Error: Follower like counts are not numeric. Skipping percentile analysis.")
-    elif len(follower_like_counts) == 0:
+    if not follower_like_counts:
         st.warning("No followers have left any likes. Skipping percentile analysis.")
     else:
         like_count_percentiles = list(set([0, 1, 5, 10, 25, 50, 75, 90, 95, 100]))
         for pct in like_count_percentiles[1:]:
             try:
                 count = follower_like_counts.quantile(pct/100)
-                pct_users = len(follower_like_counts[follower_like_counts <= count])
+                pct_users = len([value for value in follower_like_counts.values() if value <= count])
                 st.write(f"{pct_users} ({pct_users/total_followers*100:.2f}%) out of {total_followers} followers left <= {count} likes")
             except Exception as e:
                 st.warning(f"Error occurred while calculating {pct}th percentile: {e}")
-
 
 
 
